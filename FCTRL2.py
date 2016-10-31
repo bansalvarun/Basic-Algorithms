8c5fab5653eb38ea608bfdaf069787069505557c
Manav Batra <batmanav10@gmail.com>
2016-10-31 12:41:20 +0530

    Added solution to FCTRL2 from SPOJ

---
+++ FCTRL2.py
@@ -0,0 +1,11 @@
+t = int(raw_input())
+def fact(x):
+	f = 1
+	while(x):
+		f = f*x
+		x = x-1
+	return f
+while t:
+	n = int(raw_input())
+	print fact(n)
+	t = t - 1https://github.com/batmanav/Evs_project.git
